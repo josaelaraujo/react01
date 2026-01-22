a36d7f6bee0ec5ba59c10c84209084fb8a43dd86
Josael Araujo <josaelgon0123@gmail.com>
2026-01-21 21:58:30 -0300

    alterações 21/01/26

--- src/components/Condicional.js
+++ src/components/Condicional.js
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import {useState} from "react";
 
-function Condicional(){
-    const [email, serEmail] = useState()
-    const [userEmail, setUserEmail] = useState()
+function Condicional() {
+    const [email, setEmail] = useState('');
+    const [userEmail, setUserEmail] = useState('');
 
     function enviarEmail(e) {
-        e.prevenrDefault()
+        e.preventDefault()
         setUserEmail(email)
+    }
+
+    function limparEmail() {
+        setUserEmail('')
     }
 
     return(
         <div>
             <h2>Cadastre o seu e-mail</h2>
             <form>
-                <input type="email" placeholder="Digite o seu e-mail..."/>
-                <button type="submit" onClick={enviarEmail}>
+                <input type="email" placeholder="Digite o seu e-mail..." onChange={(e) => setEmail(e.target.value)}/>
+                <button type="submit" onClick = {enviarEmail} >
                     Enviar-email
                 </button>
+                {userEmail && (
+                    <div>
+                        <p>O e-mail do usuário é: {userEmail}</p>
+                        <button onClick={limparEmail}>Limpar e-mail</button>
+                    </div>
+                )}
             </form>
         </div>
     )
