6ba01ce47c3e81029002dd6e92157a205f06ac4a
Josael Araujo <josaelgon0123@gmail.com>
2026-01-25 15:18:57 -0300

    alterações 25/01/26

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Condicional from './components/Condicional';
+import { useState } from 'react'
+
+import './App.css'
+import SeuNome from './components/SeuNome'
+import Saudacao from './components/Saudacao'
 
 function App() {
+  const [nome, setNome] = useState()
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+      {/* <h1>Renderização Condicional</h1>  esse titulo é para o componente Condicional*/}
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>
     </div>
-  );
+  )
 }
 
 export default App;
